2386d2eceaeb66265f5b9dc92292244555080880
CAUÃ DE MORAES VIEIRA <caua_m_vieira@estudante.sesisenai.org.br>
2023-07-05 22:33:43 -0300

    joia

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,241 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  int leituraAmonia = 0;
+  String gravidadeAmonia = "";
 
-  // This widget is the root of your application.
+  int leituraNivelAgua = 0;
+  String gravidadeNivelAgua = "";
+
+  int leituraTemperatura = 0;
+  String gravidadeTemperatura = "";
+
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    buscarApi();
+    timer = Timer.periodic(const Duration(seconds: 30), (_) => buscarApi());
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> buscarApi() async {
+    const String apiUrl =
+        'https://api.tago.io/data?query=last_item&variables=leituratemperatura&variables=leituranivelagua&variables=leituraamonia&variables=gravidadenivelagua&variables=gravidadetemperatura&variables=gravidadeamonia';
+
+    final response = await http.get(
+      Uri.parse(apiUrl),
+      headers: {
+        HttpHeaders.authorizationHeader: '0bf84c95-fa9d-4f40-9389-4e6a65001b4a',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseBody = json.decode(response.body);
+
+      setState(() {
+        leituraTemperatura = responseBody['result'][0]['value'];
+        leituraNivelAgua = responseBody['result'][1]['value'];
+        leituraAmonia = responseBody['result'][2]['value'];
+
+        gravidadeNivelAgua = "${responseBody['result'][3]['value']}";
+        gravidadeTemperatura = "${responseBody['result'][4]['value']}";
+        gravidadeAmonia = "${responseBody['result'][5]['value']}";
+      });
+    } else {
+      // Handle error
+      print('Error: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Monitoramento de Água'),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  _buildDataGauge(
+                    'Leitura de Amônia',
+                    leituraAmonia.toDouble(),
+                    gravidadeAmonia,
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.red,
+                  ),
+                  _buildDataGauge(
+                    'Leitura de Nível de Água',
+                    leituraNivelAgua.toDouble(),
+                    gravidadeNivelAgua,
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.red,
+                  ),
+                  _buildDataGauge(
+                    'Leitura de Temperatura',
+                    leituraTemperatura.toDouble(),
+                    gravidadeTemperatura,
+                    Colors.green,
+                    Colors.red,
+                    Colors.red,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildDataGauge(
+    String title,
+    double value,
+    String gravity,
+    Color idealColor,
+    Color belowColor,
+    Color aboveColor,
+  ) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            SfRadialGauge(
+              axes: <RadialAxis>[
+                RadialAxis(
+                  minimum: 0,
+                  maximum: 100,
+                  showLabels: false,
+                  showTicks: false,
+                  axisLineStyle: AxisLineStyle(
+                    thickness: 0.15,
+                    cornerStyle: CornerStyle.bothCurve,
+                    color: Colors.grey[300],
+                    thicknessUnit: GaugeSizeUnit.factor,
+                  ),
+                  radiusFactor: 0.5,
+                  pointers: <GaugePointer>[
+                    RangePointer(
+                      value: value,
+                      width: 0.15,
+                      sizeUnit: GaugeSizeUnit.factor,
+                      gradient: SweepGradient(
+                        colors: [idealColor, belowColor, aboveColor],
+                        stops: [0.25, 0.5, 0.75],
+                      ),
+                    ),
+                    MarkerPointer(
+                      value: value,
+                      color: Colors.black,
+                      markerType: MarkerType.triangle,
+                      markerHeight: 10,
+                      markerWidth: 10,
+                    ),
+                  ],
+                  annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                      widget: Container(
+                        child: Text(
+                          value.toStringAsFixed(2),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      angle: 90,
+                      positionFactor: 0.4,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            _buildRangeLabel(gravity, idealColor, belowColor, aboveColor),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRangeLabel(
+    String gravity,
+    Color idealColor,
+    Color belowColor,
+    Color aboveColor,
+  ) {
+    return Row(
+      children: [
+        _buildLabel('Ideal', idealColor),
+        _buildLabel('Abaixo', belowColor),
+        _buildLabel('Acima', aboveColor),
+      ],
+    );
+  }
+
+  Widget _buildLabel(String text, Color color) {
+    return Expanded(
+      child: Container(
+        height: 20,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
 }
