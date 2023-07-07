33bf6cc28a5f4dc8401bdf0033be213dd30afc56
CAUÃ DE MORAES VIEIRA <caua_m_vieira@estudante.sesisenai.org.br>
2023-07-06 22:23:15 -0300

    atualizado quinta na aula

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
@@ -22,6 +23,12 @@ class _MyAppState extends State<MyApp> {
 
   int leituraTemperatura = 0;
   String gravidadeTemperatura = "";
+
+  int leituraPh = 0;
+  String gravidadePh = "";
+
+  int leituraQualidadeAgua = 0;
+  String gravidadeQualidadeAgua = "";
 
   Timer? timer;
 
@@ -56,10 +63,14 @@ class _MyAppState extends State<MyApp> {
         leituraTemperatura = responseBody['result'][0]['value'];
         leituraNivelAgua = responseBody['result'][1]['value'];
         leituraAmonia = responseBody['result'][2]['value'];
+        leituraPh = responseBody['result'][6]['value'];
+        leituraQualidadeAgua = responseBody['result'][7]['value'];
 
         gravidadeNivelAgua = "${responseBody['result'][3]['value']}";
         gravidadeTemperatura = "${responseBody['result'][4]['value']}";
         gravidadeAmonia = "${responseBody['result'][5]['value']}";
+        gravidadePh = "${responseBody['result'][8]['value']}";
+        gravidadeQualidadeAgua = "${responseBody['result'][9]['value']}";
       });
     } else {
       // Handle error
@@ -69,44 +80,79 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
           title: const Text('Monitoramento de Água'),
+          centerTitle: true,
         ),
-        body: ListView(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue[200]!, Colors.white],
+              stops: const [0.2, 0.8],
+            ),
+          ),
+         child: ListView(
           children: [
             Padding(
               padding: const EdgeInsets.all(10),
-              child: GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
+              child: Column(
                 children: [
-                  _buildDataGauge(
+                  _buildDataCard(
                     'Leitura de Amônia',
                     leituraAmonia.toDouble(),
                     gravidadeAmonia,
                     Colors.green,
                     Colors.yellow,
                     Colors.red,
+                    Icons.thermostat_outlined,
                   ),
-                  _buildDataGauge(
+                  _buildDataCard(
                     'Leitura de Nível de Água',
                     leituraNivelAgua.toDouble(),
                     gravidadeNivelAgua,
                     Colors.green,
                     Colors.yellow,
                     Colors.red,
+                    Icons.waves_outlined,
                   ),
-                  _buildDataGauge(
+                  _buildDataCard(
                     'Leitura de Temperatura',
                     leituraTemperatura.toDouble(),
                     gravidadeTemperatura,
                     Colors.green,
                     Colors.red,
                     Colors.red,
+                    Icons.thermostat_outlined,
+                  ),
+                  _buildDataCard(
+                    'Leitura de pH',
+                    leituraPh.toDouble(),
+                    gravidadePh,
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.red,
+                    Icons.thermostat_outlined,
+                  ),
+                  _buildDataCard(
+                    'Leitura de Qualidade da Água',
+                    leituraQualidadeAgua.toDouble(),
+                    gravidadeQualidadeAgua,
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.red,
+                    Icons.thermostat_outlined,
                   ),
                 ],
               ),
@@ -114,87 +160,107 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       ),
-    );
+    ));
   }
 
-  Widget _buildDataGauge(
+  Widget _buildDataCard(
     String title,
     double value,
     String gravity,
     Color idealColor,
     Color belowColor,
     Color aboveColor,
+    IconData icon,
   ) {
     return Card(
       margin: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
       child: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            ListTile(
+              leading: Icon(
+                icon,
+                size: 40,
+              ),
+              title: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             const SizedBox(height: 10),
-            SfRadialGauge(
-              axes: <RadialAxis>[
-                RadialAxis(
-                  minimum: 0,
-                  maximum: 100,
-                  showLabels: false,
-                  showTicks: false,
-                  axisLineStyle: AxisLineStyle(
-                    thickness: 0.15,
-                    cornerStyle: CornerStyle.bothCurve,
-                    color: Colors.grey[300],
-                    thicknessUnit: GaugeSizeUnit.factor,
-                  ),
-                  radiusFactor: 0.5,
-                  pointers: <GaugePointer>[
-                    RangePointer(
-                      value: value,
-                      width: 0.15,
-                      sizeUnit: GaugeSizeUnit.factor,
-                      gradient: SweepGradient(
-                        colors: [idealColor, belowColor, aboveColor],
-                        stops: [0.25, 0.5, 0.75],
-                      ),
-                    ),
-                    MarkerPointer(
-                      value: value,
-                      color: Colors.black,
-                      markerType: MarkerType.triangle,
-                      markerHeight: 10,
-                      markerWidth: 10,
-                    ),
-                  ],
-                  annotations: <GaugeAnnotation>[
-                    GaugeAnnotation(
-                      widget: Container(
-                        child: Text(
-                          value.toStringAsFixed(2),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      angle: 90,
-                      positionFactor: 0.4,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            _buildRadialGauge(value, idealColor, belowColor, aboveColor),
             const SizedBox(height: 5),
             _buildRangeLabel(gravity, idealColor, belowColor, aboveColor),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildRadialGauge(
+    double value,
+    Color idealColor,
+    Color belowColor,
+    Color aboveColor,
+  ) {
+    return SfRadialGauge(
+      axes: <RadialAxis>[
+        RadialAxis(
+          minimum: 0,
+          maximum: 100,
+          showLabels: false,
+          showTicks: false,
+          axisLineStyle: AxisLineStyle(
+            thickness: 0.15,
+            cornerStyle: CornerStyle.bothCurve,
+            color: Colors.grey[300],
+            thicknessUnit: GaugeSizeUnit.factor,
+          ),
+          radiusFactor: 0.5,
+          pointers: <GaugePointer>[
+            RangePointer(
+              value: value,
+              width: 0.15,
+              sizeUnit: GaugeSizeUnit.factor,
+              gradient: SweepGradient(
+                colors: [idealColor, belowColor, aboveColor],
+                stops: [0.25, 0.5, 0.75],
+              ),
+            ),
+            MarkerPointer(
+              value: value,
+              color: Colors.black,
+              markerType: MarkerType.triangle,
+              markerHeight: 10,
+              markerWidth: 10,
+            ),
+          ],
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+              widget: Container(
+                child: Text(
+                  value.toStringAsFixed(2),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              angle: 90,
+              positionFactor: 0.4,
+            ),
+          ],
+        ),
+      ],
     );
   }
 
@@ -212,8 +278,7 @@ class _MyAppState extends State<MyApp> {
       ],
     );
   }
-
-  Widget _buildLabel(String text, Color color) {
+    Widget _buildLabel(String text, Color color) {
     return Expanded(
       child: Container(
         height: 20,
